c2621a0a68c40aa820e2b4d38ed7bbae975bb70e
Fabiano <radbios@gmail.com>
2024-03-28 13:28:03 -0300

    feat: add var and dp calc

--- src/headers/insights.h
+++ src/headers/insights.h
@@ -6,14 +6,42 @@
 #include <stdio.h>
 #endif //STDIO_H
 
+#ifndef STRING_H
+#define STRING_H
+#include <string.h>
+#endif //STRING_H
+
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif //MATH_H
+
 typedef struct{
     float 
         sum,
         mean,
         median,
+        var,
+        dp,
         min,
         max;
 } Statistics;
+
+float calculate_dp(float var)
+{
+    return pow(var, 2);
+}
+
+float calculate_variance(float dataset[], int length, float median)
+{
+    float var = 0;
+    for (int i = 0; i < length; i++)
+    {
+        var += pow(dataset[i] - median, 2);
+    }
+
+    return var = var/length;
+}
 
 Statistics calculate_statistics(float dataset[], int length)
 {
@@ -31,6 +59,8 @@ Statistics calculate_statistics(float dataset[], int length)
     }
 
     summary.mean = summary.sum/length;
+    summary.var = calculate_variance(dataset, length, summary.mean);
+    summary.dp = calculate_dp(summary.var);
 
     if(length%2 == 0) summary.median = (dataset[length/2] + dataset[(length/2) - 1])/2;
     else summary.median = dataset[length/2];
@@ -40,7 +70,7 @@ Statistics calculate_statistics(float dataset[], int length)
 
 void print_summary(Statistics summary)
 {
-    printf("min: %.2f\nmax: %.2f\nmean: %.2f\nmedian: %.2f\n",  summary.min, summary.max, summary.mean, summary.median);
+    printf("min: %.2f\nmax: %.2f\nmean: %.2f\nmedian: %.2f\nvariance: %.2f\ndp: %.2f\n",  summary.min, summary.max, summary.mean, summary.median, summary.var, summary.dp);
 }
 
 void save_data_in_file(char *filename, float dataset[], int length)
@@ -54,20 +84,34 @@ void save_data_in_file(char *filename, float dataset[], int length)
         fprintf(file, "%zu %.2f\n", i, dataset[i]);
     }
     
-
-
     fclose(file);
 }
 
-// int data[] = {1, 2, 3, 4, 5, 6};
-// int column = 2;
-// for (size_t i = 0; i < 3; i++)
+// void export_data(char *filename, float *dataset, char *headers, int rows, int columns, char *separator)
 // {
-//     for (size_t j = 0; j < 2; j++)
-//     {
-//         printf("%d ", data[i * column + j]);
+//     FILE *file;
+
+//     file = fopen(filename, "w");
+
+//     char *tok = strtok(headers, ";");
+
+//     while (tok != NULL) {
+//         fprintf(file, "%s%s", tok, separator);
+//         tok = strtok(NULL, ";");
 //     }
-//     printf("\n");
+
+//     fprintf(file, "\n");
+
+//     for (size_t i = 0; i < rows; i++)
+//     {
+//         for (size_t j = 0; j < columns; j++)
+//         {
+//             fprintf(file, "%.2f%s", dataset[i * columns + j], separator);
+//         }
+//         fprintf(file, "\n");
+//     }
+
+//     fclose(file);
 // }
 
 #endif // INSIGHTS_H
